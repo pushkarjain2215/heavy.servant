a7dcfee536a4115bc07963c2f1503b3634b8a3a5
pushkarjain2215 <108095866+pushkarjain2215@users.noreply.github.com>
2026-02-02 01:21:21 +0530

    Update index.js

--- ggsipu-backend/index.js
+++ ggsipu-backend/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 // // load credits once
 // const creditMap = loadCredits("../analytics/credits.csv");
 
